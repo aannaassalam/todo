c083ed19253c0170055552d42e6eccbe6fbc7969
Anas Alam <anasalam027@gmail.com>
2020-07-25 23:34:40 +0530

    minor changes

--- src/Components/todo.js
+++ src/Components/todo.js
@@ -36,12 +36,14 @@ export default class Todo extends React.Component{
     }
 
     handleDelete = (id) => {
-        db.collection("todo").doc(id).delete();
-        this.setState({
-            todos: this.state.todos.filter((todo) => (
-                todo.id!==id
-            ))
-        })
+        if(window.confirm("Are you sure ???")){
+            db.collection("todo").doc(id).delete();
+            this.setState({
+                todos: this.state.todos.filter((todo) => (
+                    todo.id!==id
+                ))
+            })
+        }
     }
 
     handleClick = (item) => {
@@ -63,6 +65,45 @@ export default class Todo extends React.Component{
         }
     )}
 
+    editClick = () => {
+        document.getElementsByClassName('text')[0].removeAttribute('disabled');
+        document.getElementsByClassName('text')[0].focus();
+        document.getElementsByClassName('post')[0].classList.add('fix');
+        document.getElementsByClassName('fa-check')[0].style.display = 'inline-block';
+        document.getElementsByClassName('fa-pencil')[0].style.display = 'none';
+    }
+
+    doneClick = (id) => {
+        document.getElementsByClassName('text')[0].setAttribute('disabled', 'disabled');
+        document.getElementsByClassName('text')[0].blur();
+        document.getElementsByClassName('post')[0].classList.remove('fix');
+        document.getElementsByClassName('fa-check')[0].style.display = 'none';
+        document.getElementsByClassName('fa-pencil')[0].style.display = 'inline-block';
+        var editText = document.getElementsByClassName('text')[0].value;
+        
+        if(editText!==''){
+            this.state.todos.forEach((todo) => {
+                if(todo.id===id){
+                    todo.data.taskName = editText;
+                    this.setState({
+                        todo: [...this.state.todos]
+                    })
+                }
+            })
+        }
+
+        db.collection("todo").get().then((snap)=>{
+            snap.forEach((doc)=>{
+                var obj ={};
+                obj['id'] = doc.id;
+                obj['data'] = doc.data();
+                this.setState({
+                    todos: [...this.state.todos, obj]
+                })
+            })
+        })
+    }
+
     render(){
         return(
             <div className="container">
@@ -79,8 +120,12 @@ export default class Todo extends React.Component{
                         {
                             this.state.todos.map((todo) => (
                                 <div className="post" key={todo.id}>
-                                    <p>{todo.data.taskName}</p>
-                                    <i className="far fa-trash-alt fa-1x" onClick={()=>{this.handleDelete(todo.id)}} ></i>
+                                    <input type="text" className="text" value={todo.data.taskName} disabled/>
+                                    <div className="actions">
+                                        <i className="fa fa-pencil fa-1x" onClick={this.editClick} ></i>
+                                        <i className="fa fa-check fa-1x" onClick={() => this.doneClick(todo.id)}></i>
+                                        <i className="far fa-trash-alt fa-1x" onClick={()=>{this.handleDelete(todo.id)}} ></i>
+                                    </div>
                                 </div>
                             ))
                         }
